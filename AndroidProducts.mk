5a2ba1a06af5f2054fa0e48a20a5bc0449a7ed6d
r0ddty <r0d@ukr.net>
2025-03-24 10:16:19 +0200

    products: add Redmi 10C
    Signed-off-by: r0ddty <r0d@ukr.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,10 +8,12 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/garnet/fortune_garnet.mk \
     $(LOCAL_DIR)/sapphire/fortune_sapphire.mk \
-    $(LOCAL_DIR)/topaz/fortune_topaz.mk
+    $(LOCAL_DIR)/topaz/fortune_topaz.mk \
+    $(LOCAL_DIR)/fog/fortune_fog.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     fortune_garnet-ap3a-userdebug \
     fortune_sapphire-ap3a-userdebug \
-    fortune_topaz-ap3a-userdebug
+    fortune_topaz-ap3a-userdebug \
+    fortune_fog-ap3a-userdebug
